14dbde0c76896a50f9c12d04c5edd2a646a4f91d
Nicholas Chao <ajax053@yahoo.com>
2016-04-29 21:09:28 -0400

    deleted some files

--- node.rb
+++
@@ -1,9 +0,0 @@
-class Node
-	attr_accessor "text" , "from", "to"
-	def initialize(text, from,to)
-		@text = text	
-		@from = from
-		@to = to
-	
-	end
-end
--- random.js
+++
@@ -1,9 +0,0 @@
-ar results = [];
-$.getJSON("https://www.reddit.com/r/videos/comments/4d1gq6/cops_pull_up_400_pot_plants_in_texas_park_and/.json", function (data){
-  $.each(data[1].data.children, function (i, item) {
-    var comment = item.data.body
-    var author = item.data.author
-    var postcomment = '<p>[Author]' + author + '<br>' + comment + '</p>'
-    results.push(postcomment)
-  });
-});
